e3b4f360f76931e7c13297445692b5276c25ddf5
ItsHeroPH <heroreyes1973@gmail.com>
2024-07-27 13:09:23 +0800

    2.0.0

--- src/main/java/com/github/itsheroph/hewosleep/hooks/placeholderAPI/HewoSleepPAPIExpansion.java
+++ src/main/java/com/github/itsheroph/hewosleep/hooks/placeholderAPI/HewoSleepPAPIExpansion.java
@@ -1,8 +1,12 @@
 package com.github.itsheroph.hewosleep.hooks.placeholderAPI;
 
 import com.github.itsheroph.hewosleep.HewoSleep;
+import com.github.itsheroph.hewosleep.models.SleepUser;
+import com.github.itsheroph.hewosleep.models.SleepWorld;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class HewoSleepPAPIExpansion extends PlaceholderExpansion {
 
@@ -50,4 +54,43 @@ public class HewoSleepPAPIExpansion extends PlaceholderExpansion {
 
     }
 
+    @Override
+    public @Nullable String onPlaceholderRequest(Player player, @NotNull String identifier) {
+
+        if(player == null) return null;
+
+        SleepUser user = this.plugin.getAPI().getUserManager().getUser(player);
+
+        if(user == null) return null;
+
+        SleepWorld world = user.getWorld();
+
+        identifier = identifier.toLowerCase();
+        switch(identifier) {
+
+            case "number_sleepers":
+
+                return "" + world.getSleepingUsers().size();
+
+            case "sleepers_needed":
+
+                return "" + world.getSleepersNeeded();
+
+            case "players_in_world":
+
+                return "" + world.getUsers().size();
+
+            case "buffs":
+
+                return world.getBuffConfig().getToString(world.getBuffConfig().getBuffsList());
+
+            case "debuffs":
+
+                return world.getBuffConfig().getToString(world.getBuffConfig().getDeBuffsList());
+
+        }
+
+        return null;
+
+    }
 }
